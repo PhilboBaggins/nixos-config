4d8da5c90266ada452d1709fdab7325149cead58
Phil <PhilboBaggins@users.noreply.github.com>
2024-04-01 21:44:52 +1100

    Fix path to flake config in install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ set -u
 cd "$(dirname -- "$0")"
 
 sudo cp -v configuration.nix flake.nix /etc/nixos/
-sudo nixos-rebuild switch --flake /dev/nixos#default
+sudo nixos-rebuild switch --flake /etc/nixos#default
