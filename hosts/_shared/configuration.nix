802a535842e14e4d88bd56126be9ed56cf15687f
Phil <PhilboBaggins@users.noreply.github.com>
2024-04-02 07:06:30 +1100

    Cleanup

--- hosts/_shared/configuration.nix
+++ hosts/_shared/configuration.nix
@@ -16,10 +16,9 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Enable networking
   networking.networkmanager.enable = true;
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
   time.timeZone = "Australia/Sydney";
@@ -63,16 +62,6 @@
     wget
     zsh
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
