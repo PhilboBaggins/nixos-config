707fc0dd622279833ac5b19c5425741623a4139c
Phil <PhilboBaggins@users.noreply.github.com>
2024-04-03 05:20:36 +1100

    Improve kscreenlockerrc home-manager module

--- modules/home-manager/kscreenlockerrc.nix
+++ modules/home-manager/kscreenlockerrc.nix
@@ -1,12 +1,17 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 
 let
   cfg = config.kscreenlockerrc;
 in
 {
   options.kscreenlockerrc = {
-    autolock = lib.mkEnableOption "????????????";
-    lockonresume = lib.mkEnableOption "????????????";
+    autolock = lib.mkEnableOption "Lock screen automatically after `timeout` minutes of inactivity";
+    lockonresume = lib.mkEnableOption "Lock screen automatically after waking from sleep";
+    timeout = lib.mkOption {
+      type = lib.types.int;
+      default = 5;
+      description = "Time in minutes of inactivity before the screen is locked";
+    };
   };
 
   config.home.file = {
@@ -17,6 +22,7 @@ in
       [Daemon]
       Autolock=${lib.boolToString cfg.autolock}
       LockOnResume=${lib.boolToString cfg.lockonresume}
+      Timeout=${builtins.toString cfg.timeout}
     '';
   };
 }
